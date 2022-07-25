2a534ea2e37d18854c6046d92ab3e783abda14f0
tech-kind <ken_hayakawa@tech-kind.biz>
2022-07-25 14:43:29 +0900

    プロジェクト ファイルを追加します。

---
+++ Grains/HelloGain.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrleansBasics
+{
+    public class HelloGain : Orleans.Grain, IHello
+    {
+        private readonly ILogger _logger;
+
+        public HelloGain(ILogger<HelloGain> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<string> SayHello(string greeting)
+        {
+            _logger.LogInformation($"\n SayHello message received: greeting = '{greeting}'");
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGain says: Hello!");
+        }
+    }
+}
---
+++ Silo/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using OrleansBasics;
+
+
+return RunMainAsync().Result;
+
+
+static async Task<int> RunMainAsync()
+{
+    try
+    {
+        var host = await StartSilo();
+        Console.WriteLine("\n\n Press Enter to terminate...\n\n");
+        Console.ReadLine();
+
+        await host.StopAsync();
+
+        return 0;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        return 1;
+    }
+}
+
+static async Task<ISiloHost> StartSilo()
+{
+    // define the cluster configuration
+    var builder = new SiloHostBuilder()
+        .UseLocalhostClustering()
+        .Configure<ClusterOptions>(options =>
+        {
+            options.ClusterId = "dev";
+            options.ServiceId = "OrleansBasics";
+        })
+        .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(HelloGain).Assembly).WithReferences())
+        .ConfigureLogging(logging => logging.AddConsole());
+
+    var host = builder.Build();
+    await host.StartAsync();
+    return host;
+}
